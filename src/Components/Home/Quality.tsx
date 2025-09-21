871b6f7088b15cd352cf421b9ff629b72b25a34a
smile995 <ah.smile995@gmail.com>
2025-09-21 21:56:34 +0600

    Add feature of services in home page

--- src/Components/Home/Quality.tsx
+++ src/Components/Home/Quality.tsx
@@ -1,13 +1,45 @@
 import HeaderTitle from "../shared/HeaderTitle";
-
+import { FaHome } from "react-icons/fa";
+import { HiUserGroup } from "react-icons/hi";
+import { IoDiamondSharp } from "react-icons/io5";
 const Quality = () => {
   return (
     <div className="bg-white text-black  p-5">
       <div className=" mb-10">
-
         <HeaderTitle BText="The Future of" CText="Beauty" />
       </div>
-      <div></div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+        <div>
+          <FaHome className="lg:text-6xl text-4xl  mx-auto " />
+          <h4 className="text-center font-semibold text-md lg:text-xl">
+            Ultimate Convenience
+          </h4>
+          <p className="text-gray-500 text-center">
+            Book effortlessly within 5 minutes, skip the salon visits and avoid
+            traffic
+          </p>
+        </div>
+        <div>
+          <HiUserGroup className="lg:text-6xl text-4xl  mx-auto " />
+          <h4 className="text-center font-semibold text-md lg:text-xl">
+            Accessible Luxury
+          </h4>
+          <p className="text-gray-500 text-center">
+            Enjoy top-tier services at affordable prices, provided by
+            beauticians with 10+ years of expertise.
+          </p>
+        </div>
+        <div>
+          <IoDiamondSharp className="lg:text-6xl text-4xl  mx-auto " />
+          <h4 className="text-center font-semibold text-md lg:text-xl">
+            Premium Quality & Hygiene
+          </h4>
+          <p className="text-gray-500 text-center">
+            Experience high-quality services with exclusive, single-use branded
+            products.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
